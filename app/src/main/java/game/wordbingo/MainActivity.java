719a37a163c769333cd195cd61306a702daef16c
vtyagiaggies <vivektyagi.nith@gmail.com>
2015-10-10 12:11:35 -0500

    updated scoring logic

--- app/src/main/java/game/wordbingo/MainActivity.java
+++ app/src/main/java/game/wordbingo/MainActivity.java
@@ -1,5 +1,6 @@
 package game.wordbingo;
 
+import android.graphics.Paint;
 import android.graphics.Rect;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
@@ -35,7 +36,7 @@ public class MainActivity extends ActionBarActivity {
 
     //String serverIP = "10.112.229.53";
     String serverIP = "192.168.43.31";
-    String serverPort = "8882";
+    String serverPort = "8881";
     InputStreamReader reader = null;
     BufferedReader read = null;
     PrintWriter writer = null;
@@ -57,6 +58,14 @@ public class MainActivity extends ActionBarActivity {
     private boolean timerHasStarted = false;
     private TextView timer_text;
     private TextView help_text;
+    private TextView score_text;
+
+    private TextView textView_B;
+    private TextView textView_I;
+    private TextView textView_N;
+    private TextView textView_G;
+    private TextView textView_O;
+
     private final long startTime = 5100;
     private final long interval = 1000;
     public Map<String, Integer> m = new HashMap<String, Integer>();
@@ -72,6 +81,12 @@ public class MainActivity extends ActionBarActivity {
         countDownTimer = new MalibuCountDownTimer(startTime, interval);
         timer_text.setText(String.valueOf(startTime / 1000));
         help_text = (TextView)this.findViewById(R.id.textView_Help);
+        score_text = (TextView)this.findViewById(R.id.textView_score);
+        textView_B = (TextView)this.findViewById(R.id.textView_B);
+        textView_I = (TextView)this.findViewById(R.id.textView_I);
+        textView_N = (TextView)this.findViewById(R.id.textView_N);
+        textView_G = (TextView)this.findViewById(R.id.textView_G);
+        textView_O = (TextView)this.findViewById(R.id.textView_O);
         updateButtonValues();
 
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
@@ -95,18 +110,50 @@ public class MainActivity extends ActionBarActivity {
                         }
                     }
 
-                    writer.println(tv.getText());
+
                     Log.d(TAG, "Clicked Alphabet: " + tv.getText());
                     Log.d(TAG, "Clicked Alphabet ID: " + tv.getId());
-                    if(strikedAlphabet.size() > 2) {
-                        toggleSelectAndSwipeMode();
+                    writer.println(tv.getText());
+
+                    int k =getNumberOfRowsStriked();
+                    if (k > 0){
+                        score += k*25;
+                        if(k >= 5){
+                            score += 500;       //BINGO
+                        }
+
+                        if(k>=1){
+                            textView_B.setPaintFlags(textView_B.getPaintFlags() |  Paint.STRIKE_THRU_TEXT_FLAG);
+                        }
+
+                        if(k>=2){
+                            textView_I.setPaintFlags(textView_I.getPaintFlags() |  Paint.STRIKE_THRU_TEXT_FLAG);
+                        }
+
+                        if(k>=3){
+                            textView_N.setPaintFlags(textView_N.getPaintFlags() |  Paint.STRIKE_THRU_TEXT_FLAG);
+                        }
+
+                        if(k>=4){
+                            textView_G.setPaintFlags(textView_G.getPaintFlags() |  Paint.STRIKE_THRU_TEXT_FLAG);
+                        }
+
+                        if(k>=5){
+                            textView_O.setPaintFlags(textView_O.getPaintFlags() |  Paint.STRIKE_THRU_TEXT_FLAG);
+                        }
+
                     }
-                    else{
+
+
+                    //if(strikedAlphabet.size() > 2) {
+                        toggleSelectAndSwipeMode();
+                    //}
+                    /*else{
                         countDownTimer.cancel();
                         countDownTimer = new MalibuCountDownTimer(5100, 1000);
                         timer_text.setText(String.valueOf(5100 / 1000));
                         countDownTimer.start();
-                    }
+                    }*/
                 }
                 
             }
@@ -183,12 +230,56 @@ public class MainActivity extends ActionBarActivity {
         if (isGoodWord(word)){
             int len = word.length();
             score += len*10;
-
+            score_text.setText(String.valueOf(score));
+            Log.d(TAG, "Score:" + score);
         }
     }
+
+    public int getNumberOfRowsStriked(){
+        int k =0;
+        if(strikedAlphabet.containsKey(0) && strikedAlphabet.containsKey(1) && strikedAlphabet.containsKey(2) && strikedAlphabet.containsKey(3) && strikedAlphabet.containsKey(4)){
+            k++;
+        }
+
+        if(strikedAlphabet.containsKey(5) && strikedAlphabet.containsKey(6) && strikedAlphabet.containsKey(7) && strikedAlphabet.containsKey(8) && strikedAlphabet.containsKey(9)){
+            k++;
+        }
+
+        if(strikedAlphabet.containsKey(10) && strikedAlphabet.containsKey(11) && strikedAlphabet.containsKey(12) && strikedAlphabet.containsKey(13) && strikedAlphabet.containsKey(14)){
+            k++;
+        }
+
+        if(strikedAlphabet.containsKey(15) && strikedAlphabet.containsKey(16) && strikedAlphabet.containsKey(17) && strikedAlphabet.containsKey(18) && strikedAlphabet.containsKey(19)){
+            k++;
+        }
+
+        if(strikedAlphabet.containsKey(0) && strikedAlphabet.containsKey(5) && strikedAlphabet.containsKey(10) && strikedAlphabet.containsKey(15) && strikedAlphabet.containsKey(20)){
+            k++;
+        }
+
+        if(strikedAlphabet.containsKey(1) && strikedAlphabet.containsKey(6) && strikedAlphabet.containsKey(11) && strikedAlphabet.containsKey(16) && strikedAlphabet.containsKey(21)){
+            k++;
+        }
+
+        if(strikedAlphabet.containsKey(2) && strikedAlphabet.containsKey(7) && strikedAlphabet.containsKey(12) && strikedAlphabet.containsKey(17) && strikedAlphabet.containsKey(22)){
+            k++;
+        }
+
+        if(strikedAlphabet.containsKey(3) && strikedAlphabet.containsKey(8) && strikedAlphabet.containsKey(13) && strikedAlphabet.containsKey(18) && strikedAlphabet.containsKey(23)){
+            k++;
+        }
+
+        if(strikedAlphabet.containsKey(4) && strikedAlphabet.containsKey(9) && strikedAlphabet.containsKey(14) && strikedAlphabet.containsKey(19) && strikedAlphabet.containsKey(24)){
+            k++;
+        }
+
+        return  k;
+    }
     public void toggleSelectAndSwipeMode() {
-        if (blnAplhabetSelctionMode) {
-            blnAplhabetSelctionMode = false;        //Swipe mode is on
+        if (blnAplhabetSelctionMode && strikedAlphabet.size() > 2) {
+            //if(strikedAlphabet.size() > 2) {
+                blnAplhabetSelctionMode = false;        //Swipe mode is on
+            //}
             Set keyset = strikedAlphabet.keySet();
             Iterator it = keyset.iterator();
             while(it.hasNext()){
