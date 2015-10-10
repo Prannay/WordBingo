188def391510935846fe7af91ecafcfe52276ef1
vtyagiaggies <vivektyagi.nith@gmail.com>
2015-10-10 00:02:09 -0500

    adding timer

--- app/src/main/java/game/wordbingo/MainActivity.java
+++ app/src/main/java/game/wordbingo/MainActivity.java
@@ -4,14 +4,30 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.os.CountDownTimer;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+
 
 
 public class MainActivity extends ActionBarActivity {
+
+    private MalibuCountDownTimer countDownTimer;
+    private boolean timerHasStarted = false;
+    private TextView timer_text;
+    private final long startTime = 5100;
+    private final long interval = 1000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        timer_text = (TextView) this.findViewById(R.id.textView_timer);
+        countDownTimer = new MalibuCountDownTimer(startTime, interval);
+        timer_text.setText(String.valueOf(startTime/1000));
     }
 
 
@@ -35,5 +51,49 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public  boolean onClick_Start(View v){
+        if (!timerHasStarted)
+        {
+            countDownTimer.start();
+            timerHasStarted = true;
+            //startB.setText("Start");
+        }
+        else
+        {
+
+            countDownTimer.cancel();
+            timerHasStarted = false;
+            //startB.setText("RESET");
+        }
+        return  true;
+    }
+
+
+
+    // CountDownTimer class
+    public class MalibuCountDownTimer extends CountDownTimer
+    {
+
+        public MalibuCountDownTimer(long startTime, long interval)
+        {
+            super(startTime, interval);
+        }
+
+        @Override
+        public void onFinish()
+        {
+            timer_text.setText("up!");
+            //timeElapsedView.setText("Time Elapsed: " + String.valueOf(startTime));
+        }
+
+        @Override
+        public void onTick(long millisUntilFinished)
+        {
+            timer_text.setText(String.valueOf(millisUntilFinished/1000));
+            //timeElapsed = startTime - millisUntilFinished;
+            //imeElapsedView.setText("Time Elapsed: " + String.valueOf(timeElapsed));
+        }
     }
 }
